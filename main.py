c80069dd4d3c979103fc6d81d4d1884e0e163d7d
ldotmithu <ldotmithurshan222@gmail.com>
2025-05-01 19:46:07 +0530

    add

--- main.py
+++ main.py
@@ -72,6 +72,6 @@ if "vector_store" in st.session_state:
     if user_question:
         with st.spinner("💡 Generating answer..."):
             qa = llm_chain(st.session_state.vector_store)
-            answer = qa.run(user_question)
+            result = qa(user_question)
         st.success("✅ Answer:")
-        st.write(answer)
+        st.write(result['answer'])
